10d54824acb248bb3d6786deff041864d4662afc
mozta <perezaguirrerafael@gmail.com>
2024-02-12 14:26:10 -0600

    project base

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/cuentas")
+    .then((response) => response.json())
+    .then((data) => 
+      setData(data))
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  })
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+      
+        <ul>
+          {data && data.map((cuenta) => (
+            <li key={cuenta.id_cuenta}>{cuenta.saldo}</li>
+          ))}
+        </ul>
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
